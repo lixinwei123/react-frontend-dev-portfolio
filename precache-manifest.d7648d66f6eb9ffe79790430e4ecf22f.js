0bd6f36de78a2d43b73069bcc30a4602b3311758
Kevin <lixinwei120@gmail.com>
2023-01-03 23:48:44 -0600

    Updates

---
+++ precache-manifest.d7648d66f6eb9ffe79790430e4ecf22f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d006f8784096a318d64dcc8826c3695f",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "37de50e9473e6bf2d748",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "f04ed9dffd7f87d7ef1e",
+    "url": "/react-frontend-dev-portfolio/static/css/main.a8e5979a.chunk.css"
+  },
+  {
+    "revision": "37de50e9473e6bf2d748",
+    "url": "/react-frontend-dev-portfolio/static/js/2.3b80718c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.3b80718c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f04ed9dffd7f87d7ef1e",
+    "url": "/react-frontend-dev-portfolio/static/js/main.5f1df57c.chunk.js"
+  },
+  {
+    "revision": "963e33d1b3136e2b4c6f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.546354e7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b45847601b28375243959611837ce35a.js"
+  "/react-frontend-dev-portfolio/precache-manifest.d7648d66f6eb9ffe79790430e4ecf22f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
